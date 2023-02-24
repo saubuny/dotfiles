a5e8ac144bb294d1a9e8d7b11d8ea77e30381904
saubuny <122068609+saubuny@users.noreply.github.com>
2023-02-24 08:58:24 -0500

    basic settings

---
+++ nvim/init.lua
@@ -0,0 +1,1 @@
+require("saubuny.options");
---
+++ nvim/lua/saubuny/options.lua
@@ -0,0 +1,34 @@
+-- :help options
+
+local options = {
+    -- Indentation 
+    expandtab = true;
+    smartindent = true;
+    shiftwidth = 4;
+    tabstop = 4;
+
+    -- Visuals
+    number = true;
+    numberwidth = 4;
+    relativenumber = false;
+    cmdheight = 2;
+    splitbelow = true;
+    splitright = true;
+    wrap = false;
+    showmode = true;
+    -- vim.opt.signcolumn = "yes";
+
+    -- Editing
+    fileencoding = "utf-8";
+    clipboard = "unnamedplus";
+    mouse = "c"; 
+    completeopt = { "menuone", "noselect" }; 
+}
+
+-- Let - work with word commands
+vim.cmd [[set iskeyword+=-]];
+
+-- Apply options
+for k, v in pairs(options) do
+    vim.opt[k] = v;
+end
