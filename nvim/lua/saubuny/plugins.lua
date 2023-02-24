7ed7e2b59b45140442c46259ed4cc153db54a15d
saubuny <122068609+saubuny@users.noreply.github.com>
2023-02-24 18:03:17 -0500

    some lsp setup

--- nvim/lua/saubuny/plugins.lua
+++ nvim/lua/saubuny/plugins.lua
@@ -66,6 +66,30 @@ return packer.startup(function(use)
             ts_update()
         end,
     }
+
+    -- Set up LSP
+    use {
+      'VonHeikemen/lsp-zero.nvim',
+      branch = 'v1.x',
+      requires = {
+        -- LSP Support
+        {'neovim/nvim-lspconfig'},             -- Required
+        {'williamboman/mason.nvim'},           -- Optional
+        {'williamboman/mason-lspconfig.nvim'}, -- Optional
+
+        -- Autocompletion
+        {'hrsh7th/nvim-cmp'},         -- Required
+        {'hrsh7th/cmp-nvim-lsp'},     -- Required
+        {'hrsh7th/cmp-buffer'},       -- Optional
+        {'hrsh7th/cmp-path'},         -- Optional
+        {'saadparwaiz1/cmp_luasnip'}, -- Optional
+        {'hrsh7th/cmp-nvim-lua'},     -- Optional
+
+        -- Snippets
+        {'L3MON4D3/LuaSnip'},             -- Required
+        {'rafamadriz/friendly-snippets'}, -- Optional
+      }
+    }
     
     -- Automaticall set up packer config (Put at end)
     if PACKER_BOOTSTRAP then
--- nvim/lua/saubuny/treesitter.lua
+++ nvim/lua/saubuny/treesitter.lua
@@ -1,13 +1,13 @@
 require('nvim-treesitter.configs').setup {
   -- A list of parser names, or "all" (the four listed parsers should always be installed)
-  ensure_installed = "all", 
+  ensure_installed = 'all', 
 
   -- Other settings
   sync_install = false,
   auto_install = true,
 
   -- For when ensure_installed = "all"
-  ignore_install = { "javascript" },
+  ignore_install = { },
 
   -- Highlighting settings
   highlight = {
